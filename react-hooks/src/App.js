16e51cd74c56e503745daa79785ee1d2a6544956
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-01-06 13:39:58 -0300

    Secao - 10 - React Hooks - useEffect

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,13 +2,31 @@ import React, {useState, useEffect} from 'react';
 
 function App() {
 
-    const [tarefas, setTarefas] = useState([
-
-    ]);
+    const [tarefas, setTarefas] = useState([]);
 
     const [nomes, setNomes] = useState('Ollyver');
 
     const [input, setInput] = useState('');
+
+
+    // Como se fosse o componentDidMount
+    useEffect(() => {
+        const getTarefasSetItem = localStorage.getItem('tarefasSetItem')
+
+        if(getTarefasSetItem){
+            setTarefas(JSON.parse(getTarefasSetItem))
+        }
+
+        // Como se fosse o componentWillUnmount - Quando desmonta o componente
+        //return () => {}
+
+    }, [])
+
+    // Como se fosse o componentWillMount
+    useEffect(()=> {
+            localStorage.setItem("tarefasSetItem", JSON.stringify( tarefas))
+        },[tarefas]
+    );
 
 
     const handleAdd = () =>{
@@ -17,23 +35,23 @@ function App() {
         setInput('')
     };
 
-  return (
-    <div className="App">
-        <h1>React Hooks</h1>
-        <h4>{nomes}</h4>
+    return (
+        <div className="App">
+            <h1>React Hooks</h1>
+            <h4>{nomes}</h4>
 
-        <ul>
-            {tarefas.map((item, index) =>(
-                <li key={item+index}>{item}</li>
-            ))}
-        </ul>
+            <ul>
+                {tarefas.map((item, index) =>(
+                    <li key={item+index}>{item}</li>
+                ))}
+            </ul>
 
-        <input type="text" value={input} onChange={(e) =>setInput(e.target.value) } />
+            <input type="text" value={input} onChange={(e) =>setInput(e.target.value) } />
 
-        <button type="button" onClick={handleAdd}>Adicionar Tarefa</button>
+            <button type="button" onClick={handleAdd}>Adicionar Tarefa</button>
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
