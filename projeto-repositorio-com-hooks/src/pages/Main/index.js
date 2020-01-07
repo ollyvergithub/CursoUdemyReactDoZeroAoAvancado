722ff8409c62382d8353aa3830fa05e79164e4ef
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-01-07 13:31:39 -0300

    Instalando Axios, criando service/api.js - usando async e await(dentro do useCallback), useState e useCallback(porque manipulamos o estado)

--- projeto-repositorio-com-hooks/src/pages/Main/index.js
+++ projeto-repositorio-com-hooks/src/pages/Main/index.js
@@ -1,16 +1,62 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
 import {FaGithub, FaPlus} from 'react-icons/fa'
+import Api from "../../services/api";
 
 export default function Main() {
+
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
+
+    const handleChange = (e) => {
+        setNewRepo(e.target.value)
+    };
+
+    const handleSubmit = useCallback( (e)=> {
+        e.preventDefault();
+
+        async function submit() {
+            console.log("Ollyver ", newRepo)
+            const response = await Api.get(`repos/${newRepo}`);
+
+            const data = {
+                name: response.data.full_name,
+            };
+
+            setRepositorios([...repositorios, data]);
+            setNewRepo('')
+        }
+
+        // Como se fosse o componentWillUnmount - Quando desmonta o componente
+        submit();
+
+    }, [repositorios, newRepo])
+
+    /*async function handleSubmit(e) {
+        e.preventDefault();
+        console.log("Ollyver ", newRepo)
+
+        const response = await Api.get(`repos/${newRepo}`);
+
+        const data = {
+            name: response.data.full_name,
+        };
+
+        setRepositorios([...repositorios, data])
+        setNewRepo('')
+
+    }*/
+
+
     return (
         <div className="container">
             <h1><FaGithub size={32}/> Meus Repositorios</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-row align-items-center">
                     <div className="col-12 col-md-6">
                         <label htmlFor="add_repos">Adicionar Repositórios</label>
-                        <input type="text" className="form-control mb-2" id="add_repos"/>
+                        <input onChange={handleChange} value={newRepo} type="text" className="form-control mb-2"
+                               id="add_repos"/>
                     </div>
 
                     <div className="col-auto">
