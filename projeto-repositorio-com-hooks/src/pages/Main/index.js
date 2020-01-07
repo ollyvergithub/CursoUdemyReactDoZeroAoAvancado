49a2cf005841953f4c289824666b18cd694a8559
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-01-07 10:32:54 -0300

    Seção 11 - Projeto Repositório - Criando as Rotas e Instalando e configurando o Bootstrap na index.html

---
+++ projeto-repositorio-com-hooks/src/pages/Main/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <div className="container">
+            <h1>Pagina Main</h1>
+        </div>
+    )
+
+}
---
+++ projeto-repositorio-com-hooks/src/pages/Repositorio/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Repositorio() {
+    return (
+        <div className="container">
+            <h1>Pagina Repositorio</h1>
+        </div>
+    )
+
+}
