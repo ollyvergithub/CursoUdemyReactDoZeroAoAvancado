61f5c8609ba2de68a57bdae50da0d92903564a6e
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-01-07 11:12:32 -0300

    Adicionando react-icons (Vem com várias famílias de icones) e estilizando a Main com Form

--- projeto-repositorio-com-hooks/src/pages/Main/index.js
+++ projeto-repositorio-com-hooks/src/pages/Main/index.js
@@ -1,9 +1,24 @@
 import React from "react";
+import {FaGithub, FaPlus} from 'react-icons/fa'
 
 export default function Main() {
     return (
         <div className="container">
-            <h1>Pagina Main</h1>
+            <h1><FaGithub size={32}/> Meus Repositorios</h1>
+
+            <form>
+                <div className="form-row align-items-center">
+                    <div className="col-12 col-md-6">
+                        <label htmlFor="add_repos">Adicionar Repositórios</label>
+                        <input type="text" className="form-control mb-2" id="add_repos"/>
+                    </div>
+
+                    <div className="col-auto">
+                        <button type="submit" className="btn btn-primary mt-4"><FaPlus/></button>
+                    </div>
+                </div>
+            </form>
+
         </div>
     )
 
