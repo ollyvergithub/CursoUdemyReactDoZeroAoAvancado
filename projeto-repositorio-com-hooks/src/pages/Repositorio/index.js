93735bc04b0aec588138c3398da089d9696b11f1
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-01-09 10:19:48 -0300

    Criando PAGINAÇÃO e BLOQUEANDO BOTÃO VOLTAR QUANDO PÁGINA MENOR QUE 2

--- projeto-repositorio-com-hooks/src/pages/Repositorio/index.js
+++ projeto-repositorio-com-hooks/src/pages/Repositorio/index.js
@@ -10,6 +10,35 @@ export default function Repositorio({match}) {
     const [repositorio, setRepositorio] = useState({});
     const [issues, setIssues] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1)
+
+    // Paginação
+    function handlePage(action) {
+        setPage(action === 'back' ? page - 1 : page + 1)
+    }
+
+    useEffect(() => {
+
+        async function loadIssue() {
+
+            const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+            const response = await Api.get(`/repos/${nomeRepo}/issues`, {
+                params: {
+                    state: 'open',
+                    page,
+                    per_page: 5,
+                }
+            });
+
+            setIssues(response.data)
+        }
+
+        loadIssue();
+
+
+    }, [page])
+
 
     useEffect(() => {
 
@@ -69,23 +98,34 @@ export default function Repositorio({match}) {
 
                     {issues.map((issue, index) => (
                         <li key={String(issue.id)} className="list-group-item">
-                            <img width="150" src={issue.user.avatar_url} alt={issue.user.login}/>
+                            <img width="100" src={issue.user.avatar_url} alt={issue.user.login}/>
 
                             <p><a href={issue.html_url}>{issue.title}</a></p>
 
                             {
                                 issue.labels.length > 0 ? (
-                                    issue.labels.map((label)=>(
+                                    issue.labels.map((label) => (
                                         <span key={label.id}> <strong>Label: </strong>{label.name} </span>
                                     ))
-                                ): null
+                                ) : null
                             }
-
                             <p><strong>User Login: </strong> {issue.user.login}</p>
-
                         </li>
                     ))}
                 </ul>
+
+                <div className='row'>
+                    <div className="col-12 col-6 mb-5 mt-5 d-flex justify-content-between">
+                        <button
+                            onClick={() => handlePage('back')}
+                            disabled={page < 2}
+                            className='btn btn-primary mr-1'
+                            type="button">Voltar
+                        </button>
+                        <button onClick={() => handlePage('next')} className='btn btn-primary' type="button">Próximo
+                        </button>
+                    </div>
+                </div>
 
 
             </div>
